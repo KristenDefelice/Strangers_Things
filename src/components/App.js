fd1f2897faf075e60e0dc4a81a163d332d54097b
Clayton Strickland <claytonstrickland@Claytons-MBP.attlocal.net>
2021-01-27 21:21:40 -0600

    The new commit

---
+++ src/components/App.js
@@ -0,0 +1,57 @@
+import { 
+    BrowserRouter as Router,
+    Link, 
+    Route
+} from "react-router-dom";
+
+
+import Home from './Home';
+import Post from './Post';
+import Profile from './Profile';
+import Log from "./Log";
+
+
+
+const App = () =>{
+
+return (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/post">Post</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/log">Log In</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/post">
+        <Post />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/log">
+        <Log />
+      </Route>
+    </div>
+  </Router>
+);
+
+}
+
+
+
+
+export default App;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,3 +3,11 @@
 //messages from me
 //same header as home
 //can click on post and delete or edit it
+
+import React from 'react'
+
+const Profile = () => {
+
+}
+
+export default Profile;
